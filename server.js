2d6877d7d440849a10d3714742110d281c577b7f
Robo Kirontech <robo@kirontech.com.br>
2026-08-18 21:14:00 -0300

    Add single-process arg to Puppeteer

--- server.js
+++ server.js
@@ -89,6 +89,7 @@ const client = new Client({
             '--disable-accelerated-2d-canvas',
             '--no-first-run',
             '--no-zygote',
+            '--single-process',
             '--disable-gpu'
         ]
     },
